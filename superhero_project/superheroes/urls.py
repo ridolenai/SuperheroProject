10a4cea9ee03cabbaf501530c987faf20c634af3
ridolenai <92106359+ridolenai@users.noreply.github.com>
2021-10-29 14:18:57 -0500

    Create
    Create functionality completed

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:hero_id>/', views.detail, name = 'detail')
-    
+    path('<int:hero_id>/', views.detail, name = 'detail'),
+    path('new/', views.create, name = 'create')
+
 ]
--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Superhero
 # Create your views here.
 
@@ -17,3 +18,22 @@ def detail(request, hero_id):
         'single_hero': single_hero
     }
     return render(request, 'superheroes/detail.html', context)
+
+
+def create(request):
+    if request.method == "POST":
+        # save the form contents as a new db object
+        # return to index
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+        new_hero = Superhero(name = name, alter_ego = alter_ego, primary_ability = primary, secondary_ability = secondary, catch_phrase = catchphrase)
+        new_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+
+
+
+    else:
+        return render(request, 'superheroes/create.html')
